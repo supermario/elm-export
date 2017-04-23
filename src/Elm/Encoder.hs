cd15ec021501e340d1f87dd840e8cbc9164779c1
Mario Rogic <hello@mario.net.au>
2017-04-23 18:56:16 +0100

    Use ISO-8601 date format, date's toString has some strange representation

--- src/Elm/Encoder.hs
+++ src/Elm/Encoder.hs
@@ -127,7 +127,7 @@ instance HasEncoder ElmValue where
   render _ = return "unknown"
 
 instance HasEncoderRef ElmPrimitive where
-  renderRef EDate = pure $ parens "Json.Encode.string << toString"
+  renderRef EDate = pure $ parens "Json.Encode.string << Date.Extra.toUtcIsoString"
   renderRef EUnit = pure "Json.Encode.null"
   renderRef EInt = pure "Json.Encode.int"
   renderRef EChar = pure "Json.Encode.char"
